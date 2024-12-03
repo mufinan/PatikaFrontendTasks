68f6aa719d91d109d73ede0b6f9dd22ceb751947
mufinan <mufinan@gmail.com>
2024-12-03 16:44:43 +0300

    Hafta9 ödev1

---
+++ Hafta9/Odev1/src/components/Content.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Content() {
+    return (
+        <main>
+            <p>Welcome to the Sport Center!</p>
+        </main>
+    );
+}
+
+export default Content;
